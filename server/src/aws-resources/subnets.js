0b7714e73bda03a1d39047507685e36bbea081de
Bart Briek <bart.briek@bitvavo.com>
2023-08-18 16:59:47 +0200

    Added backend functionality

--- server/src/aws-resources/subnets.js
+++ server/src/aws-resources/subnets.js
@@ -3,20 +3,29 @@ import AWS from 'aws-sdk';
 async function fetchSubnets(awsRegion) {
   const result = [];
   try {
-    const ec2 = new AWS.EC2({ region: awsRegion });
-    const response = await ec2.describeSubnets().promise();
+    let nextToken = null;
+    const params = {
+      NextToken: nextToken,
+    };
 
-    // Create response object for Frontend
-    response.Subnets.map(subnet => {
-      result.push({
-        subnetArn: subnet.SubnetArn,
-        subnetId: subnet.SubnetId,
-        availabilityZone: subnet.AvailabilityZone,
-        subnetCidrBlock: subnet.CidrBlock,
-        vpcId: subnet.VpcId,
-        tags: subnet.Tags,
+    do {
+      const ec2 = new AWS.EC2({ region: awsRegion });
+      const response = await ec2.describeSubnets(params).promise();
+
+      // Create response object for Frontend
+      response.Subnets.map(subnet => {
+        result.push({
+          subnetArn: subnet.SubnetArn,
+          subnetId: subnet.SubnetId,
+          availabilityZone: subnet.AvailabilityZone,
+          subnetCidrBlock: subnet.CidrBlock,
+          vpcId: subnet.VpcId,
+          tags: subnet.Tags,
+        });
       });
-    });
+
+      nextToken = response.NextToken;
+    } while (params.NextToken);
   } catch (error) {
     console.error('Error', error);
   }
